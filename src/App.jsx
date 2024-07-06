471cbc41744b497b844f920b89def1d11a191b94
AnishDhomase <anishdhomase@gmail.com>
2024-07-06 09:44:55 +0530

    Initial Setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { colors, Typography } from "@mui/material";
+
+function App() {
+  return (
+    <>
+      <Typography variant="h2" sx={{ color: "gray" }}>
+        Anish
+      </Typography>
+    </>
+  );
+}
+
+export default App;
